06cd9fe5581b0b08a21e4f47053c5db2e131a829
Brian O'Donnell <briancharlesod@gmail.com>
2022-06-09 11:38:01 -0400

    1

---
+++ src/main/java/com/techelevator/Snacks.java
@@ -0,0 +1,16 @@
+package com.techelevator;
+
+public class Snacks {
+
+    private String slotLocation;
+    private String productName;
+    private double price;
+    private String type;
+//    private int ammount = 5;
+
+
+
+
+
+
+}
