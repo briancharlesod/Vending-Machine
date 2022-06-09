a34fd74d236868078a4d17284d75fc932eb51487
Sherri Kinoshita <sherri.kinoshita@gmail.com>
2022-06-09 12:41:46 -0400

    getting map

--- src/main/java/com/techelevator/Snacks.java
+++ src/main/java/com/techelevator/Snacks.java
@@ -6,7 +6,7 @@ public class Snacks {
     private String productName;
     private double price;
     private String type;
-//    private int ammount = 5;
+    private int amountInStock = 5;
 
 
     public Snacks(String slotLocation, String productName, double price, String type) {
@@ -16,6 +16,24 @@ public class Snacks {
         this.type = type;
     }
 
+    public int getAmountInStock() {
+        return amountInStock;
+    }
 
+    public void purchaseItem(String itemNumber){
+    }
 
+    public String getSound(){
+        String message = "No snack found!";
+        if(type.equals("chip")){
+            message = "Crunch Crunch, Yum!";
+        } else if(type.equals("candy")){
+            message="Munch Munch, Yum!";
+        } else if(type.equals("drink")){
+            message="Glug Glug, Yum!";
+        } else if(type.equals("gum")){
+            message = "Chew Chew, Yum!";
+        }
+        return message;
+    }
 }
