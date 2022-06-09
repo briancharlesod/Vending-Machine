7bc7a19d5277239b155f9bbb29582057a5de8ecf
Brian O'Donnell <briancharlesod@gmail.com>
2022-06-09 15:32:39 -0400

    66

--- src/main/java/com/techelevator/Application.java
+++ src/main/java/com/techelevator/Application.java
@@ -10,6 +10,7 @@ public class Application {
     input.initiateFile();
     Scanner keyboard = new Scanner(System.in);
 
+    
         System.out.println(input.snacksMap.get("A1"));
         for (String i: input.snacksMap.keySet()){
             System.out.println( input.snacksMap.get(i));
