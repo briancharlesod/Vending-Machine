8d79ba11bafd8065bb75ad7129c2e52144d5a462
Brian O'Donnell <briancharlesod@gmail.com>
2022-06-12 14:40:58 -0400

    all tests working

--- src/main/java/com/techelevator/Log.java
+++ src/main/java/com/techelevator/Log.java
@@ -13,34 +13,41 @@ public class Log {
         File log = new File("log.txt");
         public String dateTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("MM/dd/yyyy h:mm:ss a"));
 
+    public void setLog(File log) {
+        this.log = log;
+    }
 
-
-        public void feedLog(double fed) {
+    public String feedLog(double fed) {
             try (PrintWriter writer = new PrintWriter(new FileWriter(log , true))) {
                 String f = "FEED MONEY: $" + fed + " $" + Balance.remainingBalance;
                 writer.println(dateTime + " " + f);
+                return f;
             } catch (Exception e) {
                 System.out.println("log not found");
             }
+            return "";
         }
 
-        public void spendLog(Snacks buy) {
+        public String spendLog(Snacks buy) {
             try (PrintWriter writer = new PrintWriter(new FileWriter(log, true))) {
                 String s = buy.getProductName() + " "  + buy.getSlotLocation() + " $" + buy.getPrice() + " $" + Balance.remainingBalance;
                 writer.println(dateTime + " " + s);
+                return s;
             } catch (Exception e) {
                 System.out.println("log not found");
             }
+            return "";
         }
 
-        public void changeLog(double change) {
+        public String changeLog(double change) {
             try (PrintWriter writer = new PrintWriter(new FileWriter(log, true))) {
                 String c = "GIVE CHANGE: $" + change + " $0.00";
                 writer.println(dateTime + " " + c);
+                return c;
             } catch (Exception e) {
                 System.out.println("log not found");
             }
-
+            return "";
         }
         public void readLog() {
 
--- src/test/java/com/techelevator/LogTest.java
+++ src/test/java/com/techelevator/LogTest.java
@@ -1,27 +1,50 @@
 package com.techelevator;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class LogTest {
     Log log = new Log();
     UserInterface user = new UserInterface();
 
+    SnacksMap snack = new SnacksMap();
+    Balance balance = new Balance();
 
-//    @Test
-//    public void spendLogTest(){
-//
-//    String output = log.changeLog(3);
-//        Assert.assertEquals("FEED ", output);
-//    }
-//
-//    @Test
-//    public void changeLogTest(){
-//
-//    }
-//
-//    @Test
-//    public void feedLogTest(){
-//
-//    }
+
+    @Before
+    public void before() {
+        snack.createMap();
+    }
+
+    @After
+    public void after() {
+        balance.setRemainingBalance(0.0);
+    }
+
+@Test
+    public void spendTest() {
+        balance.setRemainingBalance(10);
+    String output = log.spendLog(snack.snacksMap.get("B2"));
+    Assert.assertEquals("Cowtales B2 $1.5 $10.0", output);
+}
+
+@Test
+    public void feedTest() {
+
+        String output = log.feedLog(10);
+        Assert.assertEquals("FEED MONEY: $10.0 $0.0", output);
+}
+
+@Test
+    public void changeTest() {
+
+        String output = log.changeLog(7.0);
+        Assert.assertEquals("GIVE CHANGE: $7.0 $0.00", output);
+}
 }
